fe2e74633d233c0637227325b723d81d692377b7
Viral Rathod <viral.rathod@green-apex.com>
2017-03-22 20:01:25 +0530

    Delete all for new one

--- app/core/config/constant.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('app').constant('yopillz', {
-        IMAGE_PATH: 'app/assets/images/',
-        PAGINATION_TEMP: 'main/dashboard/pagination/dirPagination.tpl.html'
-    });
-
-})();
